048145af2b5674df365684977081ad1b5f25699a
Peter <ww940521@gmail.com>
2026-09-04 16:41:39 +0800

    fix: update pledge error construction

--- AltStore/My Apps/MyAppsViewController.swift
+++ AltStore/My Apps/MyAppsViewController.swift
@@ -824,7 +824,7 @@ private extension MyAppsViewController
                    let storeApp = altstoreApp.storeApp, storeApp.isPledgeRequired && !storeApp.isPledged
                 {
                     // Assume the reason there are no apps is because we are no longer pledged to AltStore beta.
-                    error = OperationError(.pledgeInactive(appName: altstoreApp.name))
+                    error = OperationError.pledgeInactive(appName: altstoreApp.name)
                 }
                 else
                 {
